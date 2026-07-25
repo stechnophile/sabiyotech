f4ab346cef513d62feea9d5035241e21ed7aed01
MAC <mac@192.168.1.7>
2026-07-25 10:03:07 +0545

    Add GoatCounter analytics

---
+++ components/GoatCounter.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import Script from "next/script";
+
+export default function GoatCounter() {
+  return (
+    <script data-goatcounter="https://yogesh.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
+  );
+}
